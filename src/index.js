dddae1ccb778db4c361afc7e48846aea8907f61a
dimasquared <dimasquared@gmail.com>
2019-03-24 23:31:45 +0300

    solution with an error'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let factorList = [];
+  let countPrime = {};
+  let newBase = base;
+  
+  for (let k = 2; k <= newBase; ) {
+    if (newBase % k == 0) {
+      factorList.push (k);
+      newBase = newBase / k;
+    }
+    else if (k == 2) ++k;
+      else k += 2;
+  }
+
+  factorList.forEach (function(a) {
+    if (countPrime[a] != undefined)
+      ++countPrime[a];
+    else
+      countPrime[a] = 1;
+  });
+
+  let zerosCount = [];
+  let zeros = 0;
+
+  for (let key in countPrime) {
+    for (let i = 1; Math.pow (key, i) <= number; i++) {
+      zeros = zeros + Math.floor (number / Math.pow (key, i));
+    }
+    zerosCount.push (Math.floor (zeros / countPrime[key]));
+  }
+
+  let result = zerosCount[0];
+  let countLength = zerosCount.length;
+
+  for (let n = 1; n < countLength; n++) {
+    if (zerosCount[n] < result) result = zerosCount[n];
+  } 
+
+  return result;
 }
